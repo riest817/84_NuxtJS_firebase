bbd41ea2c206343667aaeb6ee0063cb7ffcf08f5
riest817 <riest817@gmail.com>
2020-11-22 15:34:08 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <!--{{ users[0].id }}, {{ users[0].name }}-->
+    <ul>
+      <li v-for="user in users" :key="user.id">
+      {{ user.id}}, {{ user.name }}, {{ user.company.name }}
+      </li>
+    </ul>
+    <img src="~/assets/cat.jpg">
+  </div>
+</template>
+
+<script>
+const axios = require('axios')
+let url = 'https://jsonplaceholder.typicode.com/users'
+
+export default {
+  asyncData({ params, error }) {
+    return axios.get(url)
+      .then((res) => {
+        return { users: res.data }
+      })
+      .catch ((e => {
+        //console.log(e.response.status)
+        error({ users: e.response.status, message: 'Error!!!!!' })
+      }))
+  }
+}
+</script>
+
+<style>
+
+</style>
